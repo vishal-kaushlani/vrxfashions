f985808dfe0360c7a8a5aec10421f0a4bb0bea0d
vishal-kaushlani <vishalkaushlani2000@gmail.com>
2024-03-07 22:41:59 +0530

    create add- subcategory functionalit and subcategory list functionality

--- app/Http/Controllers/admin/SubCategoryController.php
+++ app/Http/Controllers/admin/SubCategoryController.php
@@ -3,14 +3,59 @@
 namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
 use App\Models\Category;
+use App\Models\SubCategory;
 
 class SubCategoryController extends Controller
 {
+    public function index(Request $request){
+        $sub_categories = SubCategory::select('sub_categories.*','categories.name as category_name')
+        ->latest('sub_categories.id')
+        ->join('categories','categories.id','sub_categories.category_id');
+        if(!empty($request->get('keyword'))){
+            $sub_categories = $sub_categories->where('sub_categories.name','like','%'.$request->get('keyword').'%');
+            $sub_categories = $sub_categories->orWhere('categories.name','like','%'.$request->get('keyword').'%');
+        }
+        $sub_categories = $sub_categories->paginate(10);//latest = orderBy('created_at','DESC')
+        $data['sub_categories'] = $sub_categories;
+        //dd($category);
+        return view('admin.sub_category.list',$data);
+    }
     public function create(){
         $category = Category::orderBy('name','ASC')->get();
         $data['categories'] = $category;
         return view('admin.sub_category.create',$data);
+    }
+    public function store(Request $request){
+       $validator = Validator::make($request->all(),[
+        'name' => 'required',
+        'slug' => 'required|unique:sub_categories',
+        'category' => 'required'
+       ]);
+
+       if($validator->passes() ){
+            $sub_category = new SubCategory();
+            $sub_category->name = $request->name;
+            $sub_category->slug = $request->slug;
+            $sub_category->status = $request->status;
+            $sub_category->category_id = $request->category;
+            $sub_category->save();
+
+            $request->session()->flash('success','Sub Category created successfully');
+
+            return response([
+                'status' => true,
+                'message' => 'Sub Category created successfully!'
+            ]);
+
+       }
+       else{
+        return response([
+            'status' => false,
+            'errors' => $validator->errors()
+        ]);
+       }
     }
 }
--- resources/views/admin/sub_category/create.blade.php
+++ resources/views/admin/sub_category/create.blade.php
@@ -18,50 +18,56 @@
 <section class="content">
     <!-- Default box -->
     <div class="container-fluid">
-        <div class="card">
-            <div class="card-body">
-                <div class="row">
-                    <div class="col-md-12">
-                        <div class="mb-3">
-                            <label for="name">Category</label>
-                            <select name="category" id="category" class="form-control">
-                                if($categories->isNotEmpty()){
-                                    @foreach ($categories as $category)
-                                    <option value="{{$category->id}}">{{$category->name}}</option>
-                                    @endforeach
+        <form action="" name="subCategpryForm" id="subCategpryForm">
+            <div class="card">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="mb-3">
+                                <label for="name">Select a Category</label>
+                                <select name="category" id="category" class="form-control">
+                                    <option value="">Choose a category</option>
+                                    if($categories->isNotEmpty()){
+                                        @foreach ($categories as $category)
+                                        <option value="{{$category->id}}">{{$category->name}}</option>
+                                        @endforeach
 
-                                }
-                            </select>
+                                    }
+                                </select>
+                                <p></p>
+                            </div>
                         </div>
-                    </div>
-                    <div class="col-md-6">
-                        <div class="mb-3">
-                            <label for="name">Name</label>
-                            <input type="text" name="name" id="name" class="form-control" placeholder="Name">
+                        <div class="col-md-6">
+                            <div class="mb-3">
+                                <label for="name">Name</label>
+                                <input type="text" name="name" id="name" class="form-control" placeholder="Name">
+                                <p></p>
+                            </div>
                         </div>
-                    </div>
-                    <div class="col-md-6">
-                        <div class="mb-3">
-                            <label for="slug">Slug</label>
-                            <input type="text" name="slug" id="slug" class="form-control" placeholder="Slug">
+                        <div class="col-md-6">
+                            <div class="mb-3">
+                                <label for="slug">Slug</label>
+                                <input type="text" readonly name="slug" id="slug" class="form-control" placeholder="Slug">
+                                <p></p>
+                            </div>
                         </div>
-                    </div>
-                    <div class="col-md-6">
-                        <div class="mb-3">
-                            <label for="email">Status</label>
-                            <select name="status" id="status" class="form-control">
-                                <option value="1">Active</option>
-                                <option value="0">Block</option>
-                            </select>
+                        <div class="col-md-6">
+                            <div class="mb-3">
+                                <label for="email">Status</label>
+                                <select name="status" id="status" class="form-control">
+                                    <option value="1">Active</option>
+                                    <option value="0">Block</option>
+                                </select>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="pb-5 pt-3">
-            <button class="btn btn-primary">Create</button>
-            <a href="subcategory.html" class="btn btn-outline-dark ml-3">Cancel</a>
-        </div>
+            <div class="pb-5 pt-3">
+                <button type="submit" class="btn btn-primary">Create</button>
+                <a href="subcategory.html" class="btn btn-outline-dark ml-3">Cancel</a>
+            </div>
+        </form>
     </div>
     <!-- /.card -->
 </section>
@@ -69,6 +75,70 @@
 @endsection
 @section('customJS')
 <script>
+      $('#subCategpryForm').submit(function(event){
+        event.preventDefault();
+        var element = $(this)
+        $('button[type=submit]').prop('disabled',true);
+        $.ajax({
+            url:'{{route("sub_categories.store")}}',
+            type:'post',
+            data: element.serializeArray(),
+            dataType:'json',
+            success:function(response){
+                console.log(response)
+                $('button[type=submit]').prop('disabled',false);
+                if(response['status']==true){
+                    console.log(1)
+                    window.location.href="{{ route('sub_categories.index')}}";
+                    $('#slug').removeClass('is-invalid' )
+                        .siblings('p')
+                        .removeClass('invalid-feedback')
+                    $('#name').removeClass('is-invalid')
+                        .siblings('p')
+                        .removeClass('invalid-feedback').html('');
+                }
+                else{
+                    console.log(2)
+                    var errors = response['errors'];
+                    console.log(errors)
+                    console.log(errors);
+                    if(errors['name']){
+                        $('#name').addClass('is-invalid')
+                        .siblings('p')
+                        .addClass('invalid-feedback').html(errors['name']);
+                    }
+                    else{
+                        $('#name').removeClass('is-invalid')
+                        .siblings('p')
+                        .removeClass('invalid-feedback').html('');
+                    }
+                    if(errors['slug']){
+                        $('#slug').addClass('is-invalid')
+                        .siblings('p')
+                        .addClass('invalid-feedback').html(errors['slug']);
+                    }
+                    else{
+                        $('#slug').removeClass('is-invalid' )
+                        .siblings('p')
+                        .removeClass('invalid-feedback')
+                    }
+                    if(errors['category']){
+                        $('#category').addClass('is-invalid')
+                        .siblings('p')
+                        .addClass('invalid-feedback').html(errors['slug']);
+                    }
+                    else{
+                        $('#category').removeClass('is-invalid' )
+                        .siblings('p')
+                        .removeClass('invalid-feedback')
+                    }
+                }
+
+            },error: function(jqXHR, exception){
+                console.log("something went wrong");
+            }
+        })
+    })
 $('#name').change(function(){
     var element = $(this);
     $.ajax({
